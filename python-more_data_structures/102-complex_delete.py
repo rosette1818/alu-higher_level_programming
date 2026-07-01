ab8d8b68d316a54a51f909f1dacd1e890fce3d70
Rosette Umurerwa <r.umurerwa@alustudent.com>
2026-07-01 12:09:00 +0200

    Refactor list comprehension for keys to delete

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -4,15 +4,15 @@
 
 def complex_delete(a_dictionary, value):
     """Delete all keys with a specific value in a dictionary.
+
     Args:
         a_dictionary (dict): the dictionary.
         value: the value to search for and delete.
+
     Returns:
         dict: the dictionary with matching keys removed.
     """
-   keys_to_delete = [
-        key for key, val in a_dictionary.items() if val == value
-    ]
+    keys_to_delete = [k for k, v in a_dictionary.items() if v == value]
     for key in keys_to_delete:
         del a_dictionary[key]
     return a_dictionary

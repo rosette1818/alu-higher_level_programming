245a50f1e9e3f05867924739cf7c0843e015eab3
Rosette Umurerwa <r.umurerwa@alustudent.com>
2026-07-13 22:23:39 +0200

    4th changes to file 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -6,34 +6,18 @@ class Student:
     """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        """Initialize a new Student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-
-        If attrs is a list of strings, only represent those attributes.
-
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
-        if type(attrs) is list and all(type(item) is str for item in attrs):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        """Get a dictionary representation of the Student."""
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
         return self.__dict__
 
     def reload_from_json(self, json):
-        """Replace all attributes of the Student.
-
-        Args:
-            json (dict): The key/value pairs to replace attributes with.
-        """
+        """Replace all attributes of the Student."""
         for key, value in json.items():
             setattr(self, key, value)

55e3b7b6d628ba531cdfec413a15fd4f3fc396d6
Rosette Umurerwa <r.umurerwa@alustudent.com>
2026-07-13 19:34:04 +0200

    updating file 101 again
    Removed __setattr__ method to restrict dynamic attribute creation.

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -7,11 +7,4 @@ class LockedClass:
     except for the attribute first_name.
     """
 
-    def __setattr__(self, name, value):
-        """Set an attribute, only allowing first_name to be
-        created dynamically.
-        """
-        if name != "first_name" and name not in self.__dict__:
-            raise AttributeError("'{}' object has no attribute '{}'"
-                                  .format(type(self).__name__, name))
-        super().__setattr__(name, value)
+    __slots__ = ['first_name']

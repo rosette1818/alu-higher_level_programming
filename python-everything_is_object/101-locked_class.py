d599ad77beeb295fefc9214d770cd267696e4f35
Rosette Umurerwa <r.umurerwa@alustudent.com>
2026-07-13 19:31:58 +0200

    updating file 101
    Implement __setattr__ method to restrict attribute creation.

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
+"""Module that defines the LockedClass class."""
+
+
 class LockedClass:
-    __slots__ = ['first_name']
+    """Prevent the dynamic creation of new instance attributes,
+    except for the attribute first_name.
+    """
+
+    def __setattr__(self, name, value):
+        """Set an attribute, only allowing first_name to be
+        created dynamically.
+        """
+        if name != "first_name" and name not in self.__dict__:
+            raise AttributeError("'{}' object has no attribute '{}'"
+                                  .format(type(self).__name__, name))
+        super().__setattr__(name, value)

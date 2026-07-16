40ef9e869437c7a1f8ed64ff4cdb7d6de2e9e03b
Rosette Umurerwa <r.umurerwa@alustudent.com>
2026-07-16 14:14:16 +0200

    adding changes again to file 100
    Removed CHARACTER SET specification for the name field.

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -8,4 +8,4 @@ USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- converts the name field specifically to utf8mb4/utf8mb4_unicode_ci
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;

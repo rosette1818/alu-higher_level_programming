1172466c33be8171bc2ee38438df2a705f296796
Rosette Umurerwa <r.umurerwa@alustudent.com>
2026-07-16 12:50:51 +0200

    adding chnages to file 100

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,9 @@
 -- converts hbtn_0c_0 database, first_table, and name field to utf8mb4/utf8mb4_unicode_ci
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- selects the database so the table can be altered
+USE hbtn_0c_0;
+
 -- converts first_table to utf8mb4/utf8mb4_unicode_ci
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 

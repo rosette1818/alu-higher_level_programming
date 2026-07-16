7e5158330dcbb2c7cd3aa168e9471b675a324775
rosette1818 <r.umurerwa@alustudent.com>
2026-07-16 03:47:32 -0700

    Adding 3 other files

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,8 @@
+-- converts hbtn_0c_0 database, first_table, and name field to utf8mb4/utf8mb4_unicode_ci
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- converts first_table to utf8mb4/utf8mb4_unicode_ci
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- converts the name field specifically to utf8mb4/utf8mb4_unicode_ci
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

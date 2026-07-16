5136b6db5e62598851a3b3169aab60911cd8691d
rosette1818 <r.umurerwa@alustudent.com>
2026-07-16 10:45:21 -0700

    Addinf 4 more files

---
+++ SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,2 @@
+-- lists all genres not linked to the show Dexter
+SELECT tv_genres.name FROM tv_genres WHERE tv_genres.id NOT IN (SELECT tv_show_genres.genre_id FROM tv_show_genres WHERE tv_show_genres.show_id = (SELECT tv_shows.id FROM tv_shows WHERE tv_shows.title = "Dexter")) ORDER BY tv_genres.name ASC;
---
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,2 @@
+-- lists all shows without the genre Comedy
+SELECT tv_shows.title FROM tv_shows WHERE tv_shows.id NOT IN (SELECT tv_show_genres.show_id FROM tv_show_genres WHERE tv_show_genres.genre_id = (SELECT tv_genres.id FROM tv_genres WHERE tv_genres.name = "Comedy")) ORDER BY tv_shows.title ASC;

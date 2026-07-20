ec25212bacb2182ddf3aff97292d2fbf2f13611a
Rosette Umurerwa <r.umurerwa@alustudent.com>
2026-07-20 21:30:49 +0200

    Update 4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to the given URL with a custom header and print the body
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Send a GET request to the given URL with a header value (default 98)
+curl -s -H "X-HolbertonSchool-User-Id: ${2:-98}" "$1"

5206f3a1d8c077b9c2adbc54148217cbc74c6864
Rosette Umurerwa <r.umurerwa@alustudent.com>
2026-07-20 21:47:03 +0200

    Refactor SQL query to exclude Comedy genre shows

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -1,2 +1,6 @@
--- lists all shows without the genre Comedy
-SELECT tv_shows.title FROM tv_shows WHERE tv_shows.id NOT IN (SELECT tv_show_genres.show_id FROM tv_show_genres WHERE tv_show_genres.genre_id = (SELECT tv_genres.id FROM tv_genres WHERE tv_genres.name = "Comedy")) ORDER BY tv_shows.title ASC;
+-- lists all shows without the genre Comedy, using a single SELECT with joins
+SELECT title FROM tv_shows AS t
+LEFT JOIN tv_show_genres AS tsg ON t.id = tsg.show_id
+LEFT JOIN tv_genres AS tg ON tsg.genre_id = tg.id AND tg.name = "Comedy"
+WHERE tg.id IS NULL
+ORDER BY title;

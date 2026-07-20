857309b8981ea8ab48ddbbb430dad565bddcd57b
Rosette Umurerwa <r.umurerwa@alustudent.com>
2026-07-20 17:50:48 +0200

    adding changes to file 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
--- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- show grants for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- show grants for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';

7fb04baef4ce6a2026e4e4ddaec0ff489987d15a
Rosette Umurerwa <r.umurerwa@alustudent.com>
2026-07-20 18:03:05 +0200

    Add privilege listing for MySQL users
    Added script to list privileges for users user_0d_1 and user_0d_2.

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,7 @@
--- Creates the MySQL server user user_0d_1 with all privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+-- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on the server (in localhost).
+
+-- Lists all privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Lists all privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

a933df5f4ed2a39e23a99830e46afd7e37bea113
Rosette Umurerwa <r.umurerwa@alustudent.com>
2026-07-22 12:38:27 +0200

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
--- creates the MySQL server user user_0d_1 with all privileges
+-- Creates the MySQL user user_0d_1 with all privileges
+-- Script does not fail if the user already exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;

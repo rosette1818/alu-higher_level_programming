5b07db1631a7ebfee707a4c111a0c39d9b9fd273
Rosette Umurerwa <r.umurerwa@alustudent.com>
2026-07-22 13:53:27 +0200

    Update URL in 4-hbtn_status.py to correct endpoint

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Fetch https://alu-intranet.hbtn.io/status using the requests package.
+"""Fetch https://intranet.hbtn.io/status using the requests package.
 
 This module fetches the status endpoint and displays the type and
 content of the response body.
@@ -8,7 +8,7 @@ import requests
 
 
 if __name__ == "__main__":
-    response = requests.get("https://alu-intranet.hbtn.io/status")
+    response = requests.get("https://intranet.hbtn.io/status")
     body = response.text
     print("Body response:")
     print("\t- type: {}".format(type(body)))

a3edfe184b2e3aa8aa910c3544e14b6d5b311ae3
Rosette Umurerwa <r.umurerwa@alustudent.com>
2026-07-28 14:25:33 +0200

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,3 @@
--- This is the first file --
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
-
-CREATE USER 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

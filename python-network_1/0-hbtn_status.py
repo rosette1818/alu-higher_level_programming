17b792ab86a2b21f131b82af07738d75ab942196
rosette1818 <r.umurerwa@alustudent.com>
2026-07-28 03:21:16 -0700

    Adding changes to file 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Fetch https://alu-intranet.hbtn.io/status using the urllib package.
+"""Fetch https://intranet.hbtn.io/status using the urllib package.
 
 This module fetches the status endpoint and displays the raw body
 of the response along with its type and its utf-8 decoded content.
@@ -8,7 +8,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
+    url = "https://intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
